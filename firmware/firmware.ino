bc48b8b32ebb926cbc9df1f16f9d3cf198cf721b
juli-re <julianniklasreiser@gmail.com>
2026-06-05 11:56:57 +0200

    added button for testing

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,6 +2,7 @@
 #include <AsyncUDP.h>
 
 const char* ssid = "Silberpfeil";
+const int BUTTON_PIN = 13;
 
 AsyncUDP udp;
 const int udpPort = 8092;
@@ -9,12 +10,17 @@ const int udpPort = 8092;
 void setup() {
     WiFi.softAP(ssid);
     randomSeed(analogRead(0));
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
+  int buttonPressed = (digitalRead(BUTTON_PIN) == LOW) ? 1 : 0;
   int rpm = random(1500, 9500);         
   float oil_temp = random(700, 1100) / 10.0; 
-  String data = "{\"rpm\":" + String(rpm) + ",\"temp\":" + String(oil_temp) + "}";
+  String data = "{\"rpm\":" + String(rpm) +
+                ",\"temp\":" + String(oil_temp) +
+                ",\"button\":" + String(buttonPressed) +
+                "}";
   udp.broadcastTo(data.c_str(), udpPort);
-  delay(100); // 10 Hz
+  delay(200); // 10 Hz
 }
