c3d2b1594a4a0f244b3e463ef35b260c3822b7cb
juli-re <julianniklasreiser@gmail.com>
2026-06-11 20:23:41 +0200

    added speed ISR and calculation for rear axle speed

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,48 +2,61 @@
 #include <AsyncUDP.h>
 
 // wifi
-
 const char* ssid = "Silberpfeil";
 
 // pin setup
-
 const int MAGNET_COUNT = 6;
+const float wheelDiameter = 0.55; // in meters
 const int HALL_SENSOR_PIN = 12;
+const int SPEED_SENSOR_PIN = 14;
 const int BUTTON_PIN = 13;
 
-// isr variables
+// --- RPM ISR variables ---
+volatile uint32_t rpmPulseCount = 0;
+volatile uint32_t rpmLastPulseTime = 0;
+volatile uint32_t rpmCurrentPulseTime = 0;
+portMUX_TYPE RPM_mux = portMUX_INITIALIZER_UNLOCKED;
 
-volatile uint32_t pulseCount = 0;
-volatile uint32_t lastPulseTime = 0;
-volatile uint32_t currentPulseTime = 0;
+// --- Speed ISR variables ---
+volatile uint32_t speedPulseCount = 0;
+volatile uint32_t speedLastPulseTime = 0;
+volatile uint32_t speedCurrentPulseTime = 0;
+portMUX_TYPE Speed_mux = portMUX_INITIALIZER_UNLOCKED;
 
 AsyncUDP udp;
 const int udpPort = 8092;
 
-struct __attribute__((packed)) TelemetryPacket {
+struct __attribute__((packed)) TelemetryPacket { // 11 Bytes
     uint16_t rpm;        // 2 Bytes
     float temperature;   // 4 Bytes
+    float speed;         // 4 Bytes
     uint8_t buttonState; // 1 Byte
 };
 
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-
 void IRAM_ATTR RPM_interrupt_handle (){
-    portENTER_CRITICAL_ISR(&mux);
-    pulseCount++;
-    lastPulseTime = currentPulseTime;
-    currentPulseTime = micros();
-    portEXIT_CRITICAL_ISR(&mux);
+    portENTER_CRITICAL_ISR(&RPM_mux);
+    rpmPulseCount++;
+    rpmLastPulseTime = rpmCurrentPulseTime;
+    rpmCurrentPulseTime = micros();
+    portEXIT_CRITICAL_ISR(&RPM_mux);
+}
+
+void IRAM_ATTR SPEED_interrupt_handle (){
+    portENTER_CRITICAL_ISR(&Speed_mux);
+    speedPulseCount++;
+    speedLastPulseTime = speedCurrentPulseTime;
+    speedCurrentPulseTime = micros();
+    portEXIT_CRITICAL_ISR(&Speed_mux);
 }
 
 uint16_t calculateRPM (){
     uint32_t lastTime;
     uint32_t currentTime;
     
-    portENTER_CRITICAL(&mux);
-    lastTime = lastPulseTime;
-    currentTime = currentPulseTime;
-    portEXIT_CRITICAL(&mux);
+    portENTER_CRITICAL(&RPM_mux);
+    lastTime = rpmLastPulseTime;
+    currentTime = rpmCurrentPulseTime;
+    portEXIT_CRITICAL(&RPM_mux);
     
     uint32_t timeElapsed = micros() - currentTime;
     
@@ -56,7 +69,7 @@ uint16_t calculateRPM (){
         return 0;
     }
     
-    float calculatedRpm = (60000000.0 / pulsePeriod) / MAGNET_COUNT;
+    float calculatedRpm = (60000000.0 / pulsePeriod);
     
     if (calculatedRpm > 9000.0) {
         return 9000;
@@ -65,25 +78,61 @@ uint16_t calculateRPM (){
     return (uint16_t)calculatedRpm;
 }
 
+float calculateSpeed (){
+    uint32_t lastTime;
+    uint32_t currentTime;
+    
+    portENTER_CRITICAL(&Speed_mux);
+    lastTime = speedLastPulseTime;
+    currentTime = speedCurrentPulseTime;
+    portEXIT_CRITICAL(&Speed_mux);
+    
+    uint32_t timeElapsed = micros() - currentTime;
+    
+    if (timeElapsed > 1000000) {
+        return 0.0;
+    }
+    
+    uint32_t pulsePeriod = currentTime - lastTime;
+    if (pulsePeriod == 0) {
+        return 0.0;
+    }
+    
+    float axleRpm = (60000000.0 / pulsePeriod) / MAGNET_COUNT;
+    float currentSpeed = (axleRpm * 3.1415926535 * wheelDiameter * 60.0) / 1000.0;
+    
+    if (currentSpeed > 100.0) {
+        return 100.0;
+    }
+    
+    return currentSpeed;
+}
+
 void setup() {
     WiFi.softAP(ssid);
     randomSeed(analogRead(0));
+    
     pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(HALL_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(SPEED_SENSOR_PIN, INPUT_PULLUP);
+    
     attachInterrupt(digitalPinToInterrupt(HALL_SENSOR_PIN), RPM_interrupt_handle, FALLING);
+    attachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN), SPEED_interrupt_handle, FALLING);
 }
 
 void loop() {
-
     int buttonPressed = (digitalRead(BUTTON_PIN) == LOW) ? 1 : 0;
     uint16_t rpm = calculateRPM();         
+    float speed = calculateSpeed();
     float oil_temp = random(700, 1100) / 10.0; 
 
     TelemetryPacket packet;
     packet.rpm = rpm;
     packet.temperature = oil_temp;
+    packet.speed = speed;
     packet.buttonState = (uint8_t)buttonPressed;
 
     udp.broadcastTo((uint8_t*)&packet, sizeof(packet), udpPort);
 
     delay(20); 
-} 
+}
